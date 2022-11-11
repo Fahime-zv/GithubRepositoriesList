856e1e757b5dba332d0abf15f5b549bd5faa4c3d
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-11 21:01:11 +0330

    BaseViewModels and BaseViewModel added. This Bases for handling that view need to change ui state .for example: Loading, ErrorR ,..

---
+++ app/src/main/java/com/fahimezv/githubrepositorylist/presentation/architecture/BaseFragmentVMState.kt
@@ -0,0 +1,41 @@
+
+package com.fahimezv.githubrepositorylist.presentation.architecture
+
+import android.os.Bundle
+import android.service.controls.ControlsProviderService.TAG
+import android.util.Log
+import android.view.View
+import androidx.annotation.CallSuper
+
+/**
+ * BaseFragmentVMState, where all [UiState] get observed.
+ * You should extend this fragment whenever you have a Fragment with a ViewModel that may change the
+ * [UiState].
+ * */
+abstract class BaseFragmentVMState<V : View, VM : BaseViewModelState> : BaseFragmentVM<V, VM>() {
+
+    abstract override val viewModel: VM
+
+    @CallSuper
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (view is ViewState) {
+            viewModel.getUiStateLiveData().observe(viewLifecycleOwner) {
+                Log.d(TAG, "UiState: $it")
+                when (it) {
+                    UiState.Loading -> view.onLoading()
+                    UiState.Empty -> view.onEmpty()
+                    UiState.Data -> view.onData()
+                    UiState.NetworkError -> view.onNetworkError()
+                    is UiState.ApiError -> view.onApiError(
+                        it.msg ?: "An Error Occurred"
+                    )
+                }
+            }
+        } else {
+            throw RuntimeException("Your view must implement ViewState interface!")
+        }
+
+    }
+
+}
