7e3c7f4e991273e252eda0b173e7611d123bdb39
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-14 08:52:22 +0330

    Define the classes need Injection in moulds.

--- app/src/main/java/com/fahimezv/githubrepositorylist/core/coreModule.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/core/coreModule.kt
@@ -1,7 +1,18 @@
 package com.fahimezv.githubrepositorylist.core
 
+import com.fahimezv.githubrepositorylist.core.util.ModelUtils
+import com.google.gson.GsonBuilder
 import org.koin.dsl.module
 
 val coreModule = module {
+    //****************************************
+    //                  Utils                *
+    //****************************************
+    factory {
+        GsonBuilder()
+    }
 
+    factory {
+        ModelUtils(builder = get())
+    }
 }
--- app/src/main/java/com/fahimezv/githubrepositorylist/data/dataModule.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/dataModule.kt
@@ -1,7 +1,49 @@
 package com.fahimezv.githubrepositorylist.data
 
+import com.fahimezv.githubrepositorylist.data.network.generator.*
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.BuildConfig
 import org.koin.dsl.module
 
 val dataModule = module {
+    //***************************************
+    //               Network                *
+    //***************************************
+
+    // ------------- Generators -------------
+
+    factory {
+        if (BuildConfig.DEBUG) {    // development build
+            HttpLoggingInterceptor.Level.BODY
+        } else {    // production build
+            HttpLoggingInterceptor.Level.BASIC
+        }
+    }
+
+    single {
+        val networkEnvironment: NetworkEnvironment = NetworkEnvironmentImpl()
+        networkEnvironment
+    }
+
+    factory {
+        val networkHeadersGenerator: NetworkHeadersGenerator = NetworkHeaderGeneratorImpl()
+        networkHeadersGenerator
+    }
+
+
+
+    single {
+        val networkServiceFactory: NetworkServiceFactory = NetworkServiceFactoryImpl(
+            converterFactory = get(),
+            logLevel = get(),
+            environment = get(),
+            networkHeadersGenerator = get()
+        )
+        networkServiceFactory
+    }
+
+    factory {
+        NetworkEnvironmentImpl()
+    }
 
 }
