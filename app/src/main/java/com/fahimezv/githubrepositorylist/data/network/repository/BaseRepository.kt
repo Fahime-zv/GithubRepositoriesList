0f252ba68bbf4108716cd95d78d52d498ef5bca7
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-15 07:13:28 +0330

    Base Repository Added.
    To convert server models to our models.

---
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/repository/BaseRepository.kt
@@ -0,0 +1,23 @@
+package com.fahimezv.githubrepositorylist.data.network.repository
+
+import android.util.Log
+import com.fahimezv.githubrepositorylist.core.entity.NetworkReceivedModel
+import com.fahimezv.githubrepositorylist.data.network.model.dao.MappableDAO
+import com.fahimezv.githubrepositorylist.data.network.model.Result
+import com.fahimezv.githubrepositorylist.presentation.extentions.TAG
+
+abstract class BaseRepository {
+
+    inline fun <reified M : NetworkReceivedModel, T : MappableDAO<M>> safeApiCall(apiCall: () -> T): Result<M> {
+        // Calling the apiCall method that passed as a higher order function
+        val result = try {
+            val response = apiCall.invoke()
+            Log.i(TAG, "safeApiCall: $response")
+            Result.Data(response.map())
+        } catch (e: Throwable) { // If response code is not 200 then an exception will be thrown
+            Log.e(TAG, "${e.message}")
+            Result.NetworkError(e)
+        }
+        return result
+    }
+}
