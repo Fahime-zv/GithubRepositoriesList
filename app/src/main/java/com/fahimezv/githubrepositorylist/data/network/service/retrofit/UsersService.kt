cb0f059351fc6e42935b7cca51e6e7f970fac276
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-15 08:53:17 +0330

    Request repo added to UsersService.kt

--- app/src/main/java/com/fahimezv/githubrepositorylist/data/network/service/retrofit/UsersService.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/service/retrofit/UsersService.kt
@@ -1,4 +1,12 @@
 package com.fahimezv.githubrepositorylist.data.network.service.retrofit
 
+import com.fahimezv.githubrepositorylist.data.network.model.dao.RepoDAO
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface UsersService {
+    @GET("users/{username}/repo")
+    suspend fun repos(
+        @Path("username") userName: String
+    ):RepoDAO
 }
