9ab687879a5741acf2f5f3280796a663d6e6e6ef
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-17 17:57:12 +0330

    UserService  adn UserRepository Updated.

--- app/src/main/java/com/fahimezv/githubrepositorylist/data/network/repository/UsersRepository.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/repository/UsersRepository.kt
@@ -1,16 +1,18 @@
 package com.fahimezv.githubrepositorylist.data.network.repository
 
+import android.util.Log
 import com.fahimezv.githubrepositorylist.core.entity.Repo
 import com.fahimezv.githubrepositorylist.data.network.model.Result
+import com.fahimezv.githubrepositorylist.data.network.model.dao.RepoDAO
 import com.fahimezv.githubrepositorylist.data.network.service.retrofit.UsersService
+import com.fahimezv.githubrepositorylist.presentation.extentions.TAG
 
 class UsersRepository(
     private val service: UsersService,
 ) : BaseRepository() {
-
-    suspend fun repos(userName: String): Result<Repo> {
+    suspend fun repos(userName: String,pageIndex:Int?): Result<List<RepoDAO>> {
         return safeApiCall {
-            service.repos(userName)
+            service.repos(userName,pageIndex)
         }
     }
 }
--- app/src/main/java/com/fahimezv/githubrepositorylist/data/network/service/retrofit/UsersService.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/service/retrofit/UsersService.kt
@@ -1,12 +1,15 @@
 package com.fahimezv.githubrepositorylist.data.network.service.retrofit
 
+import com.fahimezv.githubrepositorylist.core.entity.Repo
 import com.fahimezv.githubrepositorylist.data.network.model.dao.RepoDAO
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface UsersService {
-    @GET("users/{username}/repo")
+    @GET("users/{username}/repos")
     suspend fun repos(
         @Path("username") userName: String,
-    ): RepoDAO
+        @Query("per_page") page:Int?
+    ): List<RepoDAO>
 }
