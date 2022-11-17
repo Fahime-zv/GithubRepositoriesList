b6dce0ad01cd0102864e524d7c68e08a1e1bc043
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-17 17:55:44 +0330

    BaseRepository base on received callback changed.

--- app/src/main/java/com/fahimezv/githubrepositorylist/data/network/repository/BaseRepository.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/repository/BaseRepository.kt
@@ -1,23 +1,34 @@
 package com.fahimezv.githubrepositorylist.data.network.repository
 
 import android.util.Log
-import com.fahimezv.githubrepositorylist.core.entity.NetworkReceivedModel
-import com.fahimezv.githubrepositorylist.data.network.model.dao.MappableDAO
 import com.fahimezv.githubrepositorylist.data.network.model.Result
 import com.fahimezv.githubrepositorylist.presentation.extentions.TAG
 
 abstract class BaseRepository {
-
-    inline fun <reified M : NetworkReceivedModel, T : MappableDAO<M>> safeApiCall(apiCall: () -> T): Result<M> {
+//
+    inline fun <reified M : Any> safeApiCall(apiCall: () -> M): Result<M> {
         // Calling the apiCall method that passed as a higher order function
         val result = try {
             val response = apiCall.invoke()
-            println( "safeApiCall: $response")
-            Result.Data(response.map())
+            Result.Data(response)
         } catch (e: Throwable) { // If response code is not 200 then an exception will be thrown
-           println( "${e.message}")
+            Log.e(TAG, "${e.message}")
             Result.NetworkError(e)
         }
         return result
     }
+
+//    inline fun <reified M : List<NetworkReceivedModel>, T : MappableListDAO<M>> safeApiCall(apiCall: () -> T): Result<M> {
+//        // Calling the apiCall method that passed as a higher order function
+//        val result = try {
+//            val response = apiCall.invoke()
+//            Log.i(TAG, "safeApiCall: $response")
+//           Result.Data( response.map() )
+//        } catch (e: Throwable) { // If response code is not 200 then an exception will be thrown
+//            Log.e(TAG, "${e.message}")
+//            Result.NetworkError(e)
+//        }
+//        return result
+//    }
+
 }
