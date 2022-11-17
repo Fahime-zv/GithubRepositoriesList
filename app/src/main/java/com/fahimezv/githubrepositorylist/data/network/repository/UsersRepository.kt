c07b9e7616bf0d1bc14460f8c7759b1433dd8b7c
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-17 21:40:45 +0330

    CHanged UserRepository

--- app/src/main/java/com/fahimezv/githubrepositorylist/data/network/repository/UsersRepository.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/repository/UsersRepository.kt
@@ -1,18 +1,15 @@
 package com.fahimezv.githubrepositorylist.data.network.repository
 
-import android.util.Log
-import com.fahimezv.githubrepositorylist.core.entity.Repo
 import com.fahimezv.githubrepositorylist.data.network.model.Result
 import com.fahimezv.githubrepositorylist.data.network.model.dao.RepoDAO
 import com.fahimezv.githubrepositorylist.data.network.service.retrofit.UsersService
-import com.fahimezv.githubrepositorylist.presentation.extentions.TAG
 
 class UsersRepository(
     private val service: UsersService,
 ) : BaseRepository() {
-    suspend fun repos(userName: String,pageIndex:Int?): Result<List<RepoDAO>> {
+    suspend fun repos(userName: String, pageIndex: Int, pageSize: Int): Result<List<RepoDAO>> {
         return safeApiCall {
-            service.repos(userName,pageIndex)
+            service.repos(userName, "updated","all",pageIndex, pageSize)
         }
     }
 }
