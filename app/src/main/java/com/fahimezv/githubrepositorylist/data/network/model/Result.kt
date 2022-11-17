bfa69ce700d702532a45963681aa44fec2d7b67c
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-17 18:47:05 +0330

    Result Class Changed.

--- app/src/main/java/com/fahimezv/githubrepositorylist/data/network/model/Result.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/model/Result.kt
@@ -1,14 +1,13 @@
 package com.fahimezv.githubrepositorylist.data.network.model
 
-import com.fahimezv.githubrepositorylist.core.entity.NetworkReceivedModel
 
 sealed class Result<out T : Any> {
-    data class Data<out T : NetworkReceivedModel>(val model: T) : Result<T>()
+    data class Data<out T : Any>(val model: T) : Result<T>()
     data class NetworkError(val e: Throwable) : Result<Nothing>()
 
     override fun toString(): String {
         return when (this) {
-            is Data -> "Data(model=$model)"
+            is Data<*> -> "Data(model=${model})"
             is NetworkError -> "NetworkError(e= $e)"
         }
     }
