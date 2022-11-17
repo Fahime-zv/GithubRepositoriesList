af06927bb78beca4a1fafae3c55ff121100c11ea
Fahime Zivdar <F.zivdar@nizek.com>
2022-11-17 20:55:59 +0330

    Change in api RepoService.

--- app/src/main/java/com/fahimezv/githubrepositorylist/data/network/service/retrofit/ReposService.kt
+++ app/src/main/java/com/fahimezv/githubrepositorylist/data/network/service/retrofit/ReposService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ReposService {
-    @GET("users/repos/{username}/{repository}/events")
+    @GET("repos/{username}/{repository}/events")
     suspend fun events(
         @Path("username") userName: String,
         @Path("repository") repository: String,
